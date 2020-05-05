d4f4fef5e96b04aca8be0b9fe73c2dc259a32a2e
VRBOX\GateKeeper <nikhil@doublestrike.com>
2020-05-05 15:14:47 -0400

    Added build automation to produce consistently-named release ZIP on build.

--- PluginUnixTimeConverter/AssemblyInfo.cs
+++ PluginUnixTimeConverter/AssemblyInfo.cs
@@ -1,11 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyProduct("Unix Time Converter")]
+[assembly: AssemblyTitle("Unix Time Converter for Rainmeter")]
+[assembly: AssemblyDescription("A Rainmeter plugin that converts Unix timestamps into human-readable text")]
+[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyCopyright("© 2020 - Nikhil S. Shringarpurey")]
 [assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 #if X64
 [assembly: AssemblyInformationalVersion("3.0.2.2161 (64-bit)")]
 #else
 [assembly: AssemblyInformationalVersion("3.0.2.2161 (32-bit)")]
 #endif
-[assembly: AssemblyProduct("Rainmeter")]
