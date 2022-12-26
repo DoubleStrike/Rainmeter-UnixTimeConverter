1e163cdd6761284f02f7dcb963c4abb9517ac371
VRBOX\GateKeeper <nikhil@doublestrike.com>
2022-12-26 12:06:03 -0500

    Fixed wrong version string

--- PluginUnixTimeConverter/AssemblyInfo.cs
+++ PluginUnixTimeConverter/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
 #if X64
-[assembly: AssemblyInformationalVersion("3.0.2.2161 (64-bit)")]
+[assembly: AssemblyInformationalVersion("1.1.0.0 (64-bit)")]
 #else
-[assembly: AssemblyInformationalVersion("3.0.2.2161 (32-bit)")]
+[assembly: AssemblyInformationalVersion("1.1.0.0 (32-bit)")]
 #endif
